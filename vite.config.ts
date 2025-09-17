9a2aa18d7c28bd119f5f19fab5e5051388a5c07a
Иларион Ерченко <turolretar@gmail.com>
2025-09-17 22:42:09 +0300

    :sparkles: add ability to serve app at base URL using `BASE_URL` env variable

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
+import process from 'node:process'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+    base: process.env.BASE_URL ?? '/',
+    plugins: [vue()],
 })
