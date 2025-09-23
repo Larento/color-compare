f4ee2af95a4a2bb72fb54abc88d92d792eba07ac
Иларион Ерченко <turolretar@gmail.com>
2025-09-23 21:09:16 +0300

    :sparkles: add gettext plugin to Vue app

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import { createGettext } from 'vue3-gettext'
 
-createApp(App).mount(document.body)
+import App from './App.vue'
+import translations from './language/translations.json'
+
+const gettext = createGettext({
+    availableLanguages: {
+        en: 'English',
+        ru: 'Русский',
+    },
+    defaultLanguage: 'en',
+    translations: translations,
+})
+
+createApp(App).use(gettext).mount(document.body)
